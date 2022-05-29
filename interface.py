c1ec4173cdec70c4beeeadfd52a024e81caccf16
rosi <929228573@qq.com>
2022-05-30 02:24:49 +0800

    train

--- interface.py
+++ interface.py
@@ -90,7 +90,44 @@ def train(net, train_iter, valid_iter, num_epochs, lr, loss, wd, devices, lr_per
     print(measures + f'\n{metric[2] * num_epochs / timer.sum():.1f}'
                      f' examples/sec on {str(devices)}')
 
-
+def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices, lr_period,
+          lr_decay):
+    trainer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9,
+                              weight_decay=wd)
+    loss = nn.CrossEntropyLoss(reduction="none")
+    # 等间隔调整学习率，调整倍数为gamma倍
+    scheduler = torch.optim.lr_scheduler.StepLR(trainer, lr_period, lr_decay)
+    num_batches, timer = len(train_iter), d2l.Timer()
+    legend = ['train loss', 'train acc']
+    if valid_iter is not None:
+        legend.append('valid acc')
+    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
+                            legend=legend)
+    # 用多个GPU来加速训练
+    net = nn.DataParallel(net, device_ids=devices).to(devices[0])
+    for epoch in range(num_epochs):
+        net.train()
+        metric = d2l.Accumulator(3)
+        for i, (features, labels) in enumerate(train_iter):
+            timer.start()
+            l, acc = d2l.train_batch_ch13(net, features, labels, loss,
+                                          trainer, devices)
+            metric.add(l, acc, labels.shape[0])
+            timer.stop()
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(
+                    epoch + (i + 1) / num_batches,
+                    (metric[0] / metric[2], metric[1] / metric[2], None))
+        if valid_iter is not None:
+            valid_acc = d2l.evaluate_accuracy_gpu(net, valid_iter)
+            animator.add(epoch + 1, (None, None, valid_acc))
+        scheduler.step()
+    measures = (f'train loss {metric[0] / metric[2]:.3f}, '
+                f'train acc {metric[1] / metric[2]:.3f}')
+    if valid_iter is not None:
+        measures += f', valid acc {valid_acc:.3f}'
+    print(measures + f'\n{metric[2] * num_epochs / timer.sum():.1f}'
+                     f' examples/sec on {str(devices)}')
 def evaluate_accuracy_gpu(net, data_iter, loss, device=None):
     if isinstance(net, nn.Module):
         net.eval()
