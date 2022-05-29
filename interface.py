34b9830b83e2b30b6307bc1868230351ef5acb7d
rosi <929228573@qq.com>
2022-05-30 02:21:21 +0800

    data augmentation

--- interface.py
+++ interface.py
@@ -4,7 +4,31 @@ from torch import nn
 from torch.utils import data
 from d2l import torch as d2l
 
+def load_data_cifar100(batch_size=128):
+    transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(40),
+        torchvision.transforms.RandomResizedCrop(32, scale=(0.64, 1.0),
+                                                 ratio=(1.0, 1.0)),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
+                                         [0.2023, 0.1994, 0.2010])])
 
+    transform_test = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
+                                         [0.2023, 0.1994, 0.2010])])
+
+    trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
+                                             download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=0)
+
+    testset = torchvision.datasets.CIFAR100(root='./data', train=False,
+                                            download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=0)
+    return trainloader, testloader
 def load_data_cifar100(tf_train, tf_test, batch_size=128):
     trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
                                              download=True, transform=tf_train)
