5bb6550331ff4bb3109c32b14fc48ec95ab23b79
rosi <929228573@qq.com>
2022-06-03 00:20:59 +0800

    sigmoid

---
+++ coordatt.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import math
+import torch.nn.functional as F
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
